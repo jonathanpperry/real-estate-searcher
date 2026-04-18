d22532c70d5775dce3d70be9b7c69cc7f0d80522
Jon Perry <jonperry113@gmail.com>
2026-04-18 14:09:39 -0400

    Finished building home page

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { property } from "./schemas/property";
+import { agent } from "./schemas/agent";
+import { lead } from "./schemas/lead";
+import { user } from "./schemas/user";
+import { amenity } from "./schemas/amenity";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [property, agent, lead, user, amenity],
+};
